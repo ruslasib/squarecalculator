f98dad2c152f6f22d4002fb21c96f288aa9e414a
rusla <rusla.sib@gmail.com>
2021-02-02 15:32:57 +0300

    initial commit

---
+++ src/test/java/app/SquareCalculator.java
@@ -0,0 +1,8 @@
+package app;
+
+public class SquareCalculator {
+
+  public double calculate(double a, double b) {
+    return a * b;
+  }
+}
---
+++ src/test/java/tests/SquareTests.java
@@ -0,0 +1,24 @@
+package tests;
+
+import app.SquareCalculator;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SquareTests {
+
+  private SquareCalculator calc;
+
+  @BeforeClass
+  public void precondition() {
+    calc = new SquareCalculator();
+  }
+
+  @Test
+  public void testPositive() {
+    assertThat(calc.calculate(2.0, 3.0), equalTo(6.0));
+  }
+
+}
